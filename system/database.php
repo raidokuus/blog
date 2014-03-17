3bf93f68497c9bf6fa2a83050339d3d57a254def
raidokuus <raido.kuus,001@khk.ee>
2014-03-17 15:58:46 +0200

    Fix broken links (fixes #1)

--- system/database.php
+++ system/database.php
@@ -37,20 +37,20 @@ function q($sql, & $query_pointer = NULL, $debug = FALSE)
 
 function get_one($sql, $debug = FALSE)
 {
-	global $db;
+    global $db;
 
-	if ($debug) { // kui debug on TRUE
-		print "<pre>$sql</pre>";
-	}
-	switch (substr($sql, 0, 6)) {
-		case 'SELECT':
-			$q = mysqli_query($db, $sql) or db_error_out();
-			return mysqli_num_rows($q) ? mysqli_fetch_assoc($q) : NULL;
-		default:
-			exit('get_one("' . $sql . '") failed because get_one expects SELECT statement.');
-	}
+    if ($debug) { // kui debug on TRUE
+        print "<pre>$sql</pre>";
+    }
+    switch (substr($sql, 0, 6)) {
+        case 'SELECT':
+            $q = mysqli_query($db, $sql) or db_error_out();
+            $result = mysqli_fetch_array($q);
+            return empty($result) ? NULL: $result[0];
+        default:
+            exit('get_one("' . $sql . '") failed because get_one expects SELECT statement.');
+    }
 }
-
 function get_all($sql)
 {
 	global $db;
